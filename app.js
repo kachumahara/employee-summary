44ff74455943f8d7e4db2bbd44db892dd6e350bf
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-03 09:10:35 -0700

    created output for team html and also finished the app js codes

--- app.js
+++ app.js
@@ -25,3 +25,11 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an 
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
+
+const html = render([
+    new Manager("Michael", 1, "michael@micharl.com", 214),
+    new Engineer("Kenneth", 3, "kachumahara@gmail.com", "kachumahara"),
+    new Intern("Trevor", "trevor@trevor.com", "UA")
+])
+console.log(html)
+fs.writeFile(outputPath, html, () => {});
