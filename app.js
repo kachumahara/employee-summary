cb546324e41dfd0f513d9c25a9d894b5bd7508de
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-10 22:54:28 -0700

    added comments

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function initialPrompt() {
       }
     });
 };
-// and to create objects for each team member (using the correct classes as blueprints!)
+// creating maanager's information
 initialPrompt();
 function addManager() {
     inquirer
@@ -69,6 +69,7 @@ function addManager() {
   initialPrompt();
 })
 }
+// Adding Engineer's information
 function addEngineer(){
   inquirer
   .prompt([
@@ -99,6 +100,7 @@ function addEngineer(){
   initialPrompt();
 })
 }
+// adding intern information
 function addIntern(){
   inquirer
   .prompt([
@@ -130,23 +132,7 @@ function addIntern(){
 })
 }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-
+/// rendering the new team members to output path (team.html)
 function exitApplication() {
   var page = render(teamMembers);
   fs.writeFile(outputPath, page, (err) => {
